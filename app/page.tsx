eeccc8a7d9c5136c910f6c000e66da01900b5b18
Akin Saye Sokpah <assokpah@gmail.com>
2026-01-04 18:00:59 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,2 @@
-
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-
-export default function Home() {
-  return (
-    <main>
-      <Navbar />
-      <section className="hero">
-        <h1>Sokpah Dev Browser</h1>
-        <p>Build. Learn. Solve.</p>
-        <a href="/subscribe"><button>Go Pro</button></a>
-      </section>
-      <Footer />
-    </main>
-  );
-}
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
